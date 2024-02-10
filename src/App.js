4846bca02e8daa92a99558a3e32a8cbca05d6628
Tidish <tidishstudio@gmail.com>
2024-02-10 18:22:11 +0200

    Spells page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import * as React from 'react';
+import Item from './Item';
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={darkTheme}>
+                
+                <Box
+                    sx={{
+                        bgcolor: 'background.default',
+                        height: '100vh',
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Typography variant="h2" gutterBottom color={'white'}>
+                        Choose Spells
+                    </Typography>
+                    <Item text={'All The Spells'} fileName={'spells-all.json'} />
+                    <Item text={'Player\'s handbook'} fileName={'spells-srd.json'} />
+                    <Item text={'Xanathar\'s Guide to Everything'} fileName={'spells-xge.json'} />
+                    <Item text={'Guildmaster\'s Guide to Ravnica'} fileName={'spells-ggr.json'} />
+                    <Item text={'Acquisitions Incorporated'} fileName={'spells-ai.json'} />
+                    <Item text={'Explorer\'s Guide to Wildemount'} fileName={'spells-egw.json'} />
+                    <Item text={'Tasha\'s Cauldron of Everything'} fileName={'spells-tce.json'} />
+                    <Item text={'Fizban\'s Treasury of Dragons'} fileName={'spells-ftd.json'} />
+                    <Item text={'Astral Adventurer\'s Guide'} fileName={'spells-aag.json'} />
+                    <Item text={'Strixhaven: A Curriculum of Chaos'} fileName={'spells-scc.json'} />
+                    <Item text={'Icewind Dale - Rime of the Frostmaiden'} fileName={'spells-idrotf.json'} />
+                </Box>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
