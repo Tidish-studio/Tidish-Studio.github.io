71a159fb76d63f39939e2b0f2f47b52fc6cd9f82
Tidish <tidishstudio@gmail.com>
2024-02-12 20:40:04 +0200

    Update routong

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import * as React from 'react';
-import {
-    Route,
-    BrowserRouter as Router,
-    Routes,
-} from "react-router-dom";
-import Navbar from "./Navbar";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
 import PrivacyPolicy from './PrivacyPolicy';
 import SpellsPage from './SpellsPage';
 
 function App() {
     return (
         <div>
-            <Router>
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={<SpellsPage />} />
-                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            </Routes>
-        </Router>
+            <HashRouter>
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<SpellsPage />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                </Routes>
+            </HashRouter>
         </div>
     );
 }
