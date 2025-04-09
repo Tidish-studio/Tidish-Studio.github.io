95d4997891413a322fa3f6ba3ec6afbc5ee95b3a
Tidish <tidishstudio@gmail.com>
2025-04-09 19:39:40 +0300

    Older spells

--- src/components/file-grid.tsx
+++ src/components/file-grid.tsx
@@ -1,7 +1,6 @@
 import { File } from "@/schema";
-import allSpells2024 from '../files/spells-all-2024.json';
-import allSpells from '../files/spells-all-new.json';
-import phbSpells from '../files/spells-phb.json';
+import allSpells2024 from '../files/older/spells-all-2024.json';
+import spells2014 from '../files/older/spells-all-new.json';
 import { FileCard } from "./file-card";
 
 interface FileGridProps {
@@ -10,10 +9,9 @@ interface FileGridProps {
 
 export function FileGrid({ files }: FileGridProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <FileCard file={files[0]} content={JSON.stringify(allSpells)} />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+        <FileCard file={files[0]} content={JSON.stringify(spells2014)} />
         <FileCard file={files[1]} content={JSON.stringify(allSpells2024)} />
-        <FileCard file={files[2]} content={JSON.stringify(phbSpells)} />
     </div>
   );
 }
--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,4 +1,21 @@
 // Static list of demo files
+// export const files = [
+//   {
+//     id: 1,
+//     name: "All The Spells With 2024",
+//     type: "application/json",
+//     description: "Acquisitions Incorporated, Astral Adventurer's Guide, Eberron: Rising from the Last War, Elemental Evil Player's Companion, Fizban's Treasury of Dragons, Guildmasters' Guide to Ravnica, Icewind Dale: Rime of the Frostmaiden, Mythic Odysseys of Theros, Player's Handbook (2014), Player's Handbook (2024), Sigil and the Outlands, Strixhaven: A Curriculum of Chaos, Sword Coast Adventurer's Guide, Tasha's Cauldron of Everything, The Book of Many Things, Volo's Guide to Monsters, Xanathar's Guide to Everything",
+//     lastUpdated: new Date("2025-02-28"),
+//   },
+//   {
+//     id: 2,
+//     name: "Partnered and Other spells",
+//     type: "application/json",
+//     description: "Dungeons & Dragons vs Rick and Morty, Dungeons of Drakkenheim, Explorer's Guide to Wildemount, Grim Hollow: Lairs of Etharis, Tal'Dorei Campaign Setting Reborn, Adventures in the Forgotten Realms: A Verdant Tomb, Adventures in the Forgotten Realms: From Cyan Depths, Lost Laboratory of Kwalish",
+//     lastUpdated: new Date("2025-02-28"),
+//   }
+// ];
+
 export const files = [
   {
     id: 1,
@@ -7,15 +24,15 @@ export const files = [
     description: "All the official spells",
     lastUpdated: new Date("2025-02-25"),
   },
+  // {
+  //   id: 2,
+  //   name: "Player's Handbook",
+  //   type: "application/json",
+  //   description: "Only PHB spells without 2024 spells",
+  //   lastUpdated: new Date("2025-02-25"),
+  // },
   {
     id: 2,
-    name: "Player's Handbook",
-    type: "application/json",
-    description: "Only PHB spells without 2024 spells",
-    lastUpdated: new Date("2025-02-25"),
-  },
-  {
-    id: 3,
     name: "All The Spells 2024",
     type: "application/json",
     description: "All The Spells with 2024 (only use with the newest app version)",
