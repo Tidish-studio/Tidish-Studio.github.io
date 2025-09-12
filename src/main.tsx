bc01bb407450c3bd69c93ac191a0c4fc52dc6c49
Tidish <tidishstudio@gmail.com>
2025-09-12 10:40:22 +0300

    Changed routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
